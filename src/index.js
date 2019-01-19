83e5232a17e1701e1b9e8c64af46b36103bc99c6
davebehr1 <davebehr1@gmail.com>
2019-01-19 22:54:48 +0200

    implement the server component and rest library to ping the servers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+const request = require('request');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+//import registerServiceWorker from './registerServiceWorker';
+var servers = ["https://cognition.dev.stackworx.cloud/api/status",
+			   "https://ord.dev.stackworx.io/health",
+			   "https://api.durf.dev.stackworx.io/health",
+			   "https://prima.run/health",
+			   "https://stackworx.io/",
+			   "https://stackworx.io/"
+			   ];			   
+class Server extends React.Component{
+	render(){
+		return(
+			<div className="server">
+			<p>{this.props.value}</p>
+			<p>{this.props.status}</p>
+			</div>
+			);
+	}
+}
+
+class ServerRack extends React.Component{
+	constructor(props){
+		super(props);
+		this.state ={
+			servers: servers,
+			active: Array(servers.length).fill("cat")
+
+		}
+	}
+	checkStatus(){
+		this.state.servers.map((item,key) => 
+
+
+		request.get(item).on('response',(response) => {
+			if(response.statusCode === 200){
+			//this.setState(active[0])
+			//console.log(this.state.active);
+			this.state.active[key] = "up";
+			}else{
+				this.state.active[key] = "down";
+			}
+			console.log(this.state.active);
+		})
+		)
+	}
+	renderServer(i){
+		for(var i =0; i < this.state.servers.length;++i){
+			return (<Server value={i}/>); 
+		}
+		//return (<Server value = {i}/>);
+	}
+	 componentDidMount() {
+      setInterval(() => this.setState({ time: Date.now()}), 10000)
+ 	}
+
+ 	componentWillUnmount() {
+ 	 clearInterval(this.interval);
+	}
+
+	render(){
+		return(
+
+			<div>	
+				{this.checkStatus()}
+				{this.state.servers.map((item,key) => 
+
+					<Server value={item} status= {this.state.active[key]} />
+				)}
+
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<ServerRack />, document.getElementById('root'));
+//registerServiceWorker();
